531e8b9eee2f8bc7a11b74356ab864b2fd0a9ac8
soapshen <soapshen99@gmail.com>
2022-08-08 15:36:26 +0800

    update curing_clustering.py

--- process_decoys.sh
+++ process_decoys.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/bash
 idx=0
 len=$(cat Diverse_ligands_len.txt)
-echo start molecular clustering
+echo start molecular curing and clustering
 
 while [ $idx -lt $len ]
 do
-    echo clustering potential decoys \for ligand_$idx, $[$len-$idx-1] ligands left
+    echo curing and clustering potential decoys \for ligand_$idx, $[$len-$idx-1] ligands left
     export idx
-    python agglomerative_clustering.py
+    python curing_clustering.py
     let "idx++"
     echo ligand_$[$idx-1] finished
 done
 
-echo molecular clustering finished
+echo molecular curing and clustering finished
 echo pooling all decoys
 
 python pool_decoys.py
 
-echo pooling finished, postprocessing finished
+echo pooling finished, refining finished
